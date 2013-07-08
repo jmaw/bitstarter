0d0f7f91138d8d5c0c3832b448cb31d743682d29
Jose Manuel Ayala Wilson <jmayalaw@gmail.com>
2013-07-08 23:41:26 +0200

    Updated web.js

--- web.js
+++ web.js
@@ -4,12 +4,12 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var data = fs.readFileSync('./index.html'); 
+  var data = fs.readFileSync('index.html'); 
   var buffer = new Buffer(data);
   response.send(buffer.toString());
 });
 
-var port = process.env.PORT || 5002;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
